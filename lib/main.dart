1ec3bbee231f01e659d20c433d09cf5a4b16ab37
shoval <shovalnaim23@gmail.com>
2023-11-21 18:51:27 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home:Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+            backgroundColor: Colors.red,
+            title:Text('Dicee',
+            style:TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            )
+          ),
+      ),
+        body: Dicee(),
+    ),
+    ),
+  );
+}
+class Dicee extends StatefulWidget {
+
+  @override
+  _DiceeState createState() => _DiceeState();
+}
+
+class _DiceeState extends State<Dicee> {
+  var leftNum=1;
+  var rightNum=1;
+
+  void returnCube(){
+    setState(() {
+      leftNum=Random().nextInt(6)+1;
+      rightNum=Random().nextInt(6)+1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(child:Row(
+      children: [
+        Expanded(
+          child: TextButton(
+            onPressed: (){
+
+                returnCube();
+
+
+            },
+            child: Image.asset('images/dicee$leftNum.png'),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: (){
+                returnCube();
+
+            },
+            child: Image.asset('images/dicee$rightNum.png'),
+
+          ),
+        )
+
+      ],
+    ),
+    );
+  }
+}
+
+
